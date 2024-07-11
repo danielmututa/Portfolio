eb6ef3d9fc38bd14902ff112b2a98f77080add5d
unknown <mututadaniel54@gmail.com>
2024-07-11 03:29:36 -0700

    Add new files and update README

---
+++ index.js
@@ -0,0 +1,19 @@
+let typed = new typed(".front",{
+    Strings:["Front-End Developer","Web Development"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
+
+
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+//  end menu
+
